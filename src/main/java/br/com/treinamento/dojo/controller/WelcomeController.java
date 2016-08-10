8afc016e469e35c6265ec08cdb9671bf0fcb49de
André Poersch <andremp@ciandt.com>
2016-08-10 18:17:24 -0300

    Spring boot configuration

--- src/main/java/br/com/treinamento/dojo/controller/WelcomeController.java
+++ src/main/java/br/com/treinamento/dojo/controller/WelcomeController.java
@@ -1,5 +1,11 @@
 package br.com.treinamento.dojo.controller;
 
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
